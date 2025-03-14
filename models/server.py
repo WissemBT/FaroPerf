a771ae1796f044f3ae9f1d0f57322f1a0b4bf42f
Wissem Ben Bettaieb <wissembettaieb2000@gmail.com>
2025-03-14 03:30:39 +0100

    correct issue with response of server creation

--- models/server.py
+++ models/server.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from database import Base
+from datetime import datetime
 import uuid
 
 
@@ -10,3 +11,4 @@ class Server(Base):
     server_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     hostname = Column(String, nullable=False)
     ip_address = Column(String, unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
