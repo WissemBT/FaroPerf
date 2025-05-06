e5c05577d9eaf99abb444e73a44b2a61915104ca
WissemBT <87197901+WissemBT@users.noreply.github.com>
2025-05-07 00:06:49 +0200

    Protect all apis (#7)
    * protect apis
    
    * ensuring ownership and good api protection to avoid bypassing rights

--- routes/metric.py
+++ routes/metric.py
@@ -5,11 +5,23 @@ from models.metric import Metric
 from schemas.metric import MetricCreate, MetricOut
 import uuid
 from datetime import datetime
+from routes.auth import get_current_user
+from models.user import User
+from models.server import Server
+
 
 router = APIRouter(prefix="/metrics", tags=["Metrics"])
 
-@router.post("/",response_model=MetricOut, status_code=status.HTTP_201_CREATED)
-def create_metric(metric_data: MetricCreate, db: Session= Depends(get_db)):
+
+@router.post("/", response_model=MetricOut, status_code=status.HTTP_201_CREATED)
+def create_metric(
+    metric_data: MetricCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+    ):
+    server = db.query(Server).filter_by(server_id=metric_data.server_id, user_id=current_user.user_id).first()
+    if not server:
+        raise HTTPException(status_code=404, detail="Server not found or not owned by you")
     new_metric = Metric(
         metric_id=str(uuid.uuid4()),
         server_id=metric_data.server_id,
@@ -18,15 +30,18 @@ def create_metric(metric_data: MetricCreate, db: Session= Depends(get_db)):
         memory_usage=metric_data.memory_usage,
         disk_usage=metric_data.disk_usage,
         network_in=metric_data.network_in,
-        network_out=metric_data.network_out
+        network_out=metric_data.network_out,
     )
-
-    db.add(new_metric)
-    db.commit()
-    db.refresh(new_metric)
+    db.add(new_metric); db.commit(); db.refresh(new_metric)
     return new_metric
 
-@router.get("/{server_id}",response_model=list[MetricOut])
-def get_metrics_for_server(server_id: uuid.UUID, db: Session = Depends(get_db)):
-    metrics = db.query(Metric).filter_by(server_id=server_id).all()
-    return metrics
+
+@router.get("/{server_id}", response_model=list[MetricOut])
+def list_metrics_for_server(
+    server_id: uuid.UUID,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+    ):
+    if not db.query(Server).filter_by(server_id=server_id, user_id=current_user.user_id).first():
+        raise HTTPException(status_code=404, detail="Server not found or not owned by you")
+    return db.query(Metric).filter_by(server_id=server_id).all()
