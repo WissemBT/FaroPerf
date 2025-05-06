4a90f39779f6096ec8340b6acf7e17d9ff5519d3
Wissem Ben Bettaieb <wissembettaieb2000@gmail.com>
2025-05-06 22:44:45 +0200

    adding sign in and sign up

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.alert import router as alert_router
 from routes.alert_rules import router as alert_rules_router
 from routes.user import router as user_router
 from routes.notification import router as notification_router
+from routes.auth import router as auth_router
 
 app = FastAPI()
 
@@ -14,7 +15,7 @@ app.include_router(alert_router)
 app.include_router(alert_rules_router)
 app.include_router(user_router)
 app.include_router(notification_router)
-
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
